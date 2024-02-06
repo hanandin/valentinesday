642723da88ba17004bbfeb174c021a7d36609d7a
unknown <hanandinunna@gmail.com>
2024-02-06 10:49:09 -0500

    Optimized button behavior for mobile devices

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [gif, setGif] = useState('https://gifdb.com/images/high/angry-animated-bears-9z416raof8j7etdu.gif'); // Replace with your initial GIF URL
-  const [showButtons, setShowButtons] = useState(true); // New state to control button visibility
+  const [gif, setGif] = useState('https://gifdb.com/images/high/angry-animated-bears-9z416raof8j7etdu.gif');
+  const [showButtons, setShowButtons] = useState(true);
   const [noButtonStyle, setNoButtonStyle] = useState({});
   const [message, setMessage] = useState('Will you be my Valentine?');
 
-  const handleNoMouseOver = () => {
+  const moveButton = () => {
     const newX = Math.random() * (window.innerWidth - 150);
     const newY = Math.random() * (window.innerHeight - 100);
     setNoButtonStyle({
@@ -18,9 +18,14 @@ function App() {
     });
   };
 
+  const handleNoClick = (event) => {
+    event.preventDefault(); // Prevents the default action on mobile
+    moveButton();
+  };
+
   const handleYesClick = () => {
-    setGif(' https://64.media.tumblr.com/63c05a6fbc22eeb8cf3301f05e714bbb/11771f8b773a21f7-ef/s400x600/7dc6f86cca535f8a532f33acfd26164cbed71758.gif'); // Replace with your new GIF URL after Yes is clicked
-    setShowButtons(false); // Set the buttons to be hidden
+    setGif('https://64.media.tumblr.com/63c05a6fbc22eeb8cf3301f05e714bbb/11771f8b773a21f7-ef/s400x600/7dc6f86cca535f8a532f33acfd26164cbed71758.gif');
+    setShowButtons(false);
     setMessage('You made my day! 💖');
   };
 
@@ -28,11 +33,11 @@ function App() {
     <div className="App">
       <div className="content">
         <img src={gif} alt="Cute bear" className="bear-gif" />
-        <p>{message}</p> {/* Use the message state here */}
-        {showButtons && ( // Only render buttons if showButtons is true
+        <p>{message}</p>
+        {showButtons && (
           <div className="button-container">
             <button onClick={handleYesClick} className="yes-button">Yes</button>
-            <button style={noButtonStyle} onMouseOver={handleNoMouseOver} className="no-button">No</button>
+            <button style={noButtonStyle} onTouchStart={handleNoClick} onMouseOver={moveButton} className="no-button">No</button>
           </div>
         )}
       </div>
